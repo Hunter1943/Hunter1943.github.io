e367577318e1a608f7a1604305dae4ff3ff4f959
jiawei.hua <jiawei.hua@meituan.com>
2024-02-14 23:58:30 +0800

    add

--- zoe/show_anim.js
+++ zoe/show_anim.js
@@ -107,6 +107,6 @@ function timeElapse(date){
     if (seconds < 10) {
         seconds = "0" + seconds;
     }
-    var result = "我们在一起 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒啦!"; 
+    var result = "我们在一起 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒啦"; 
     $("#elapseClock").html(result);
 }
